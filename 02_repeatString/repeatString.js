d58ec88a909b317f274b2121a279a95c869988fb
Marc <147423007+marcaixs@users.noreply.github.com>
2024-04-11 12:45:38 +0200

    repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let response = "";
+    for(let i = 1; i<= num; i++){
+        response += string;
+    }
+    if(num<0) response = 'ERROR';
+    return response;
 };
 
 // Do not edit below this line
