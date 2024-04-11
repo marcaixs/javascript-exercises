7dd55b2e841534a7a0e899c25c0b0eef32a8a699
Marc <147423007+marcaixs@users.noreply.github.com>
2024-04-11 12:50:23 +0200

    reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let response='';
+    for(let i = string.length - 1; i>=0; i-- ){
+        response += string.at(i);
+    }
+    return response;
 };
 
 // Do not edit below this line
